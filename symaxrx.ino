64cbbfb393a640a84810d6794e628def6dd91319
Roman Moravčík <roman.moravcik@gmail.com>
2018-01-09 08:53:56 +0100

    Updated sketch to follow real HW.

--- symaxrx.ino
+++ symaxrx.ino
@@ -1,12 +1,10 @@
-/* symax_rx.ino -- An arduino sketch to test the protocol symax
- *
- */
-
 #include <SPI.h>
-#include <symax_protocol.h>
-#include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+#include "ServoTimer1.h"
+#include "symax_protocol.h"
+
+ServoTimer1 left;
+ServoTimer1 right;
 
 nrf24l01p wireless; 
 symaxProtocol protocol;
@@ -14,16 +12,16 @@ symaxProtocol protocol;
 unsigned long time = 0;
 
 void setup() {
-
   Serial.begin(115200);
 
-  myservo.attach(8);  // attaches the servo on pin 8 to the servo object
+  left.attach(9);
+  right.attach(10);
 
   // SS pin must be set as output to set SPI to master !
   pinMode(SS, OUTPUT);
 
-  // Set CE pin to 10 and CS pin to 9
-  wireless.setPins(10,9);
+  // Set CE pin to 5 and CS pin to 4
+  wireless.setPins(5,4);
   
   // Set power (PWRLOW,PWRMEDIUM,PWRHIGH,PWRMAX)
   wireless.setPwr(PWRLOW);
@@ -32,7 +30,6 @@ void setup() {
   
   time = micros();
   Serial.println("Start");
-  
 }
 
 rx_values_t rxValues;
@@ -44,16 +41,17 @@ void loop()
   time = micros();
   uint8_t value = protocol.run(&rxValues); 
   newTime = micros();
+  int left_value, right_value;
    
-  switch( value )
+  switch (value)
   {
-    case  NOT_BOUND:
-        Serial.println("Not bound");
-    break;
-
-    case  BIND_IN_PROGRESS:
-        Serial.println("Bind in progress");
-    break;
+    case NOT_BOUND:
+      Serial.println("Not bound");
+      break;
+      
+    case BIND_IN_PROGRESS:
+      Serial.println("Bind in progress");
+      break;
     
     case BOUND_NEW_VALUES:
       Serial.print(newTime - time);
@@ -68,19 +66,31 @@ void loop()
       Serial.print("\t"); Serial.print(rxValues.picture);
       Serial.print("\t"); Serial.print(rxValues.highspeed);
       Serial.print("\t"); Serial.println(rxValues.flip);
-      //time = newTime;
 
-      myservo.write(map(rxValues.roll, -127, 127, 0, 180));              // tell servo to go to position
-      
-    break;
+      if (rxValues.roll > 0) // left
+      {
+        left_value = map(rxValues.roll, 0, 127, rxValues.throttle, 0);
+        right_value = rxValues.throttle;
+      }
+      else if (rxValues.roll < 0) // right
+      {
+        left_value = rxValues.throttle;
+        right_value = map(rxValues.roll, 0, -127, rxValues.throttle, 0);
+      }
+      else // forward
+      {
+        left_value = rxValues.throttle;
+        right_value = rxValues.throttle;
+      }
+
+      left.write(map(left_value, 0, 255, 0, 180));
+      right.write(map(right_value, 0, 255, 0, 180));
+      break;
 
     case BOUND_NO_VALUES:
-      //Serial.print(newTime - time); Serial.println(" : ----");
-    break;
+      break;
 
     default:
-    break;
-
+      break;
   }
-
 }
